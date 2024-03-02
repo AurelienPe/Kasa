10497d8ad98d382841f7ff2004bffd0ddb8ffff9
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-03-02 13:10:39 +0100

    add images

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import './style.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home'
+import Footer from './components/footer/footer'
+import Header from './components/header/header'
+import Error from './pages/404error/404error'
+import About from "./pages/about/about";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}/>
+        <Route path="/accommodationsheet" />
+        <Route path="/404error" element={<Error />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
