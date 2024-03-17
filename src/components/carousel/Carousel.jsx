6f5d42122939a40a07bf21496b2bea93f0eb3fd1
PEYRAT Aurélien <aure1814fr@gmail.com>
2024-03-17 03:36:19 +0100

    add carousel components + accomodation page

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,13 +1,31 @@
-import './carousel.css'
+import React, { useState } from 'react';
+import './carousel.css';
 
-const Carousel = (image) => {
+const Carousel = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToNextImage = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % images.length);
+  };
+
+  const goToPreviousImage = () => {
+    setCurrentImageIndex((currentImageIndex - 1 + images.length) % images.length);
+  };
 
   return (
-    <div className="carousel">
-      <img src={image} alt=''/>
+    <header className="carousel">
+      <img src={images[currentImageIndex]} alt='' />
 
-    </div>
+      <button onClick={goToPreviousImage}>
+        <i class="fa-solid fa-chevron-left"></i>
+      </button>
+
+      <button onClick={goToNextImage}>
+        <i class="fa-solid fa-chevron-right"></i>
+      </button>
+
+    </header>
   );
 }
 
-export default Carousel
+export default Carousel;
--- src/pages/accommodationpage/Accommodationpage.jsx
+++ src/pages/accommodationpage/Accommodationpage.jsx
@@ -1,19 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './accommodationpage.css';
+import Carousel from '../../components/carousel/Carousel';
+import logementsData from '../../assets/logements'
 import './accommodationpage.css'
-import Carousel from '../../components/carousel/Carousel'
+import Collapse from '../../components/collapse/Collapse';
 
-const Accommodationpage = (props) => {
-const { pictures } = props.location || {};
-const images = pictures || [];
+const Accommodationpage = () => {
+  const [accommodationData, setAccommodationData] = useState(null);
 
-return (
-  <main>
-    <Carousel
-      images={images}
-    />
-    <div>Accomodation pages</div>
-  </main>
-);
+  useEffect(() => {
+    // Extraire l'ID de l'URL
+    const pathname = window.location.pathname;
+    const id = pathname.substring(pathname.lastIndexOf('/') + 1);
+
+    // Rechercher l'objet correspondant dans les données logements.json
+    const data = logementsData.find(item => item.id === id);
+    setAccommodationData(data);
+  }, []);
+
+  return (
+    <main>
+      {accommodationData ? (
+        <>
+          <Carousel images={accommodationData.pictures} />
+          <ul className="accommodationpage">
+
+            <li>
+              <h2>{accommodationData.title}</h2>
+            </li>
+
+            <li>{accommodationData.location}</li>
+
+            <li className="tags-list">
+              {accommodationData.tags.map(tag => (
+              <p className="tag">{tag}</p>
+              ))}
+            </li>
+
+            <div>
+              <li>
+                <img src={accommodationData.host.picture} alt={accommodationData.host.name} />
+              </li>
+
+              <li className="host-name">{accommodationData.host.name}</li>
+
+              <li>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+              </li>
+            </div>
+
+            <li className="collapse">
+              <Collapse 
+              title='Description'
+              text={accommodationData.description} 
+              />
+
+              <Collapse
+              title='Équipements'
+              text={accommodationData.equipments}
+              />
+            </li>
+
+          </ul>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </main>
+  );
 }
 
-export default Accommodationpage
+export default Accommodationpage;
